f64e104126622deac43202469013242275af5a5c
leosimas <leosimasgoncalves@gmail.com>
2019-04-05 10:54:05 -0300

    first commit

---
+++ app/src/main/java/com/leosimas/mvvm/ui/main/MainViewmodel.kt
@@ -0,0 +1,10 @@
+package com.leosimas.mvvm.ui.main
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(application: Application) : AndroidViewModel(application) {
+
+
+
+}
